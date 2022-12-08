aa55314f28884a7e839ff3e337e6cf4124816dc6
Artemis4777 <cberger0354@gmail.com>
2022-12-08 12:30:39 -0500

    Fixed some formatting. Added hiding of billing.

--- static/app.js
+++ static/app.js
@@ -1,3 +1,4 @@
+//base api call
 const form = document.getElementById('paymentform');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -7,4 +8,17 @@ form.addEventListener('submit', (event) => {
   const XHR = new XMLHttpRequest();
   XHR.open("POST", "/submit");
   XHR.send(formData);
+})
+
+//hide or show billing
+const billing = document.getElementById('billing');
+const billInfo = document.getElementById('billing info')
+billing.addEventListener("change",  function(){
+  if (billing.checked){
+    console.log("showing")
+    billInfo.classList.remove("d-none");
+  }
+  else{
+    billInfo.classList.add("d-none");
+  }
 })
