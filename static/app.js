c79e45c19438ed5de1cd2abc9e7fadd5fe38675b
Artemis4777 <cberger0354@gmail.com>
2022-12-13 12:26:27 -0500

    Added iFields, but it messed up the look

--- static/app.js
+++ static/app.js
@@ -1,24 +1,36 @@
+//JavaScript functions for main page
+
 //base api call
 const form = document.getElementById('paymentform');
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  console.log(form);
-  const formData = new FormData(form);
-  console.log(formData)
-  const XHR = new XMLHttpRequest();
-  XHR.open("POST", "/submit");
-  XHR.send(formData);
+    event.preventDefault();
+    const XHR = new XMLHttpRequest();
+    let ifieldsKey = document.getElementById('ifields-key').value;
+    setAccount(ifieldsKey, "NB Everything", "1.0")
+    var submitBtn = document.getElementById('paymentbutton');
+    submitBtn.disabled = true;
+    getTokens(
+        function () {
+            const formData = new FormData(form);
+            XHR.open("POST", "/submit");
+            XHR.send(formData);
+        },
+        function () {
+            submitBtn.disabled = false;
+        },
+        30000,
+    );
 })
 
 //hide or show billing
 const billing = document.getElementById('billing');
 const billInfo = document.getElementById('billing info')
-billing.addEventListener("change",  function(){
-  if (billing.checked){
-    console.log("showing")
-    billInfo.classList.remove("d-none");
-  }
-  else{
-    billInfo.classList.add("d-none");
-  }
+billing.addEventListener("change", function () {
+    if (billing.checked) {
+        console.log("showing")
+        billInfo.classList.remove("d-none");
+    }
+    else {
+        billInfo.classList.add("d-none");
+    }
 })
