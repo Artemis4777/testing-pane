1ec154cd34de71f9939f90e8cd1b5a11497b725a
Artemis4777 <cberger0354@gmail.com>
2022-12-16 12:54:16 -0500

    Added 3DS. Not handling any results yet.

--- application.py
+++ application.py
@@ -24,6 +24,15 @@ def submit():
     cardnum = request.form.get("cc-number")
     cvv = request.form.get("cc-cvv")
     exp = request.form.get("cc-expiration")
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    address = request.form.get("address")
+    country = request.form.get("country")
+    state = request.form.get("state")
+    city = request.form.get("city")
+    zip = request.form.get("zip")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
     dupe = "true" if request.form.get("allow-duplicate") == "on" else "false"
 
     payload = json.dumps({
@@ -36,9 +45,55 @@ def submit():
         "xcardnum": cardnum,
         "xcvv": cvv,
         "xexp": exp,
-        "xallowduplicate": dupe
+        "xBillFirstName": firstname,
+        "xBillLastName": lastname,
+        "xBillStreet": address,
+        "xBillCountry": country,
+        "xBillState": state,
+        "xBillCity": city,
+        "xBillZip": zip,
+        "xEmail": email,
+        "xBillMobile": phone,
+        "xallowduplicate": dupe,
     })
     print(payload)
+    apiCall = requests.request("POST", url, headers = headers, data = payload)
+    response = apiCall.json()
+    print(response["xStatus"], response["xRefNum"])
+    return json.dumps({"Status": 200, "Response": response})
+
+
+#3D Secure friction handling
+@application.route("/verify", methods=["POST"])
+def verify():
+    print(request.form)
+    headers = {"Content-Type": "application/json"}
+    url = "https://x1.cardknox.com/verifyjson"
+    key = request.form.get("api-key")
+    dupe = "true" if request.form.get("allow-duplicate") == "on" else "false"
+    xRefnum = request.form.get("xRefNum")
+    xCavv = request.form.get("xCavv")
+    xEci = request.form.get("xEci")
+    x3dsAuthenticationStatus = request.form.get("x3dsAuthenticationStatus")
+    x3dsSignatureVerificationStatus = request.form.get("x3dsSignatureVerificationStatus")
+    x3dsActionCode = request.form.get("x3dsActionCode")
+    x3dsError = request.form.get("x3dsError")
+    print(x3dsError)
+    payload = json.dumps(
+        {
+            "xsoftwareversion": "1.0.0",
+            "xversion": "5.0.0",
+            "xsoftwarename": "NB Test",
+            "xkey": key,
+            "xallowduplicate": dupe,
+            "xRefnum": xRefnum,
+            "xCavv": xCavv,
+            "xEci": xEci,
+            "x3dsAuthenticationStatus": x3dsAuthenticationStatus,
+            "x3dsSignatureVerificationStatus": x3dsSignatureVerificationStatus,
+            "x3dsActionCode": x3dsActionCode,
+        }
+    )
     apiCall = requests.request("POST", url, headers = headers, data = payload)
     response = apiCall.json()
     print(response["xStatus"], response["xRefNum"])
--- static/app.js
+++ static/app.js
@@ -1,15 +1,26 @@
 //JavaScript functions for main page
 let styles = {
     "width": "98%",
-	"font-size": "1rem",
-	"font-weight": "400",
-	"line-height": "1.5",
-	"color": "#212529",
+    "font-size": "1rem",
+    "font-weight": "400",
+    "line-height": "1.5",
+    "color": "#212529",
     "border": "0",
 }
 setIfieldStyle('card-number', styles);
 setIfieldStyle('cvv', styles);
 
+//check if 3D Secure
+function threedstuff() {
+    if (threeD.checked) {
+        console.log("enabling 3ds with friction")
+        enable3DS(enviro, handle3DSResults);
+    }
+    else {
+        console.log("enabling 3ds with friction")
+        enable3DS(enviro)
+    }
+}
 
 //base api call
 const form = document.getElementById('paymentform');
@@ -18,13 +29,18 @@ form.addEventListener('submit', (event) => {
     const XHR = new XMLHttpRequest();
     let ifieldsKey = document.getElementById('ifields-key').value;
     setAccount(ifieldsKey, "NB Everything", "1.0")
+    threedstuff()
     var submitBtn = document.getElementById('paymentbutton');
     submitBtn.disabled = true;
     getTokens(
         function () {
             const formData = new FormData(form);
-            XHR.open("POST", "/submit");
-            XHR.send(formData);
+            fetch("/submit", {
+                method: "POST",
+                body: formData
+            })
+                .then(response => response.json())  // Parse the response as JSON
+                .then(data => console.log(data))  // Do something with the data
         },
         function () {
             submitBtn.disabled = false;
@@ -32,6 +48,26 @@ form.addEventListener('submit', (event) => {
         30000,
     );
 })
+
+//3DS result handling
+function handle3DSResults(actionCode, xCavv, xEciFlag, xRefNum, xAuthenticateStatus, xSignatureVerification) {
+    const XHR = new XMLHttpRequest();
+    var postData = {
+        xRefNum: xRefNum,
+        xCavv: xCavv,
+        xEci: xEciFlag,
+        x3dsAuthenticationStatus: xAuthenticateStatus,
+        x3dsSignatureVerificationStatus: xSignatureVerification,
+        x3dsActionCode: actionCode,
+        x3dsError: ck3DS.error
+    };
+    fetch("/verify", {
+        method: "POST",
+        body: postData
+    })
+    .then(response => response.json())  // Parse the response as JSON
+    .then(data => console.log(data))  // Do something with the data
+}
 
 //hide or show billing
 const billing = document.getElementById('billing');
@@ -43,5 +79,23 @@ billing.addEventListener("change", function () {
     }
     else {
         billInfo.classList.add("d-none");
+    }
+})
+
+//configure 3DS
+const enviro = document.getElementById('3ds-environment').value;
+const threeDe = document.getElementById('3ds-environment-d');
+const threeD = document.getElementById('enable-3ds');
+const threeDf = document.getElementById('enable-3ds-f');
+const enableFriction = document.getElementById('friction-switch');
+threeD.addEventListener("change", function () {
+    if (threeD.checked) {
+        threeDe.classList.remove("d-none");
+        enableFriction.classList.remove("d-none");
+        threeDf.checked = true
+    }
+    else {
+        threeDe.classList.add("d-none");
+        enableFriction.classList.add("d-none");
     }
 })
